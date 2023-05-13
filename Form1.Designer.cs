2fe6a61ddc839436032f01335c7425a8d11f2d65
ajai <ajaikumarvs@gmail.com>
2023-05-13 10:02:26 +0530

    Basic UI layout updated.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -30,10 +30,27 @@
         {
             components = new System.ComponentModel.Container();
             kryptonPalette1 = new ComponentFactory.Krypton.Toolkit.KryptonPalette(components);
+            flowLayoutPanel1 = new FlowLayoutPanel();
+            kryptonButton1 = new ComponentFactory.Krypton.Toolkit.KryptonButton();
+            kryptonButton2 = new ComponentFactory.Krypton.Toolkit.KryptonButton();
+            kryptonButton3 = new ComponentFactory.Krypton.Toolkit.KryptonButton();
+            kryptonButton4 = new ComponentFactory.Krypton.Toolkit.KryptonButton();
+            kryptonButton5 = new ComponentFactory.Krypton.Toolkit.KryptonButton();
+            kryptonButton6 = new ComponentFactory.Krypton.Toolkit.KryptonButton();
+            panel1 = new Panel();
+            menuStrip1 = new MenuStrip();
+            fileToolStripMenuItem = new ToolStripMenuItem();
+            editToolStripMenuItem = new ToolStripMenuItem();
+            viewToolStripMenuItem = new ToolStripMenuItem();
+            projectToolStripMenuItem = new ToolStripMenuItem();
+            toolStrip1 = new ToolStrip();
+            flowLayoutPanel1.SuspendLayout();
+            menuStrip1.SuspendLayout();
             SuspendLayout();
             // 
             // kryptonPalette1
             // 
+            kryptonPalette1.BasePaletteMode = ComponentFactory.Krypton.Toolkit.PaletteMode.ProfessionalSystem;
             kryptonPalette1.FormStyles.FormMain.StateCommon.Back.Color1 = Color.White;
             kryptonPalette1.FormStyles.FormMain.StateCommon.Back.Color2 = Color.White;
             kryptonPalette1.FormStyles.FormMain.StateCommon.Border.DrawBorders = ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Top | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Bottom | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Left | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Right;
@@ -43,13 +60,143 @@
             kryptonPalette1.HeaderStyles.HeaderForm.StateCommon.Border.Color2 = Color.White;
             kryptonPalette1.HeaderStyles.HeaderForm.StateCommon.Border.DrawBorders = ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Top | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Bottom | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Left | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Right;
             // 
+            // flowLayoutPanel1
+            // 
+            flowLayoutPanel1.Controls.Add(kryptonButton1);
+            flowLayoutPanel1.Controls.Add(kryptonButton2);
+            flowLayoutPanel1.Controls.Add(kryptonButton3);
+            flowLayoutPanel1.Controls.Add(kryptonButton4);
+            flowLayoutPanel1.Controls.Add(kryptonButton5);
+            flowLayoutPanel1.Controls.Add(kryptonButton6);
+            flowLayoutPanel1.Dock = DockStyle.Right;
+            flowLayoutPanel1.Location = new Point(792, 0);
+            flowLayoutPanel1.Name = "flowLayoutPanel1";
+            flowLayoutPanel1.Size = new Size(101, 595);
+            flowLayoutPanel1.TabIndex = 0;
+            // 
+            // kryptonButton1
+            // 
+            kryptonButton1.Dock = DockStyle.Top;
+            kryptonButton1.Location = new Point(3, 3);
+            kryptonButton1.Name = "kryptonButton1";
+            kryptonButton1.Size = new Size(98, 67);
+            kryptonButton1.TabIndex = 0;
+            kryptonButton1.Values.Text = "kryptonButton1";
+            // 
+            // kryptonButton2
+            // 
+            kryptonButton2.Dock = DockStyle.Top;
+            kryptonButton2.Location = new Point(3, 76);
+            kryptonButton2.Name = "kryptonButton2";
+            kryptonButton2.Size = new Size(98, 67);
+            kryptonButton2.TabIndex = 1;
+            kryptonButton2.Values.Text = "kryptonButton2";
+            // 
+            // kryptonButton3
+            // 
+            kryptonButton3.Dock = DockStyle.Top;
+            kryptonButton3.Location = new Point(3, 149);
+            kryptonButton3.Name = "kryptonButton3";
+            kryptonButton3.Size = new Size(98, 67);
+            kryptonButton3.TabIndex = 2;
+            kryptonButton3.Values.Text = "kryptonButton3";
+            // 
+            // kryptonButton4
+            // 
+            kryptonButton4.Dock = DockStyle.Top;
+            kryptonButton4.Location = new Point(3, 222);
+            kryptonButton4.Name = "kryptonButton4";
+            kryptonButton4.Size = new Size(98, 67);
+            kryptonButton4.TabIndex = 3;
+            kryptonButton4.Values.Text = "kryptonButton4";
+            // 
+            // kryptonButton5
+            // 
+            kryptonButton5.Dock = DockStyle.Top;
+            kryptonButton5.Location = new Point(3, 295);
+            kryptonButton5.Name = "kryptonButton5";
+            kryptonButton5.Size = new Size(98, 67);
+            kryptonButton5.TabIndex = 4;
+            kryptonButton5.Values.Text = "kryptonButton5";
+            // 
+            // kryptonButton6
+            // 
+            kryptonButton6.Dock = DockStyle.Top;
+            kryptonButton6.Location = new Point(3, 368);
+            kryptonButton6.Name = "kryptonButton6";
+            kryptonButton6.Size = new Size(98, 67);
+            kryptonButton6.TabIndex = 5;
+            kryptonButton6.Values.Text = "kryptonButton6";
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.Black;
+            panel1.Dock = DockStyle.Right;
+            panel1.Location = new Point(782, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(10, 595);
+            panel1.TabIndex = 1;
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.Dock = DockStyle.Bottom;
+            menuStrip1.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            menuStrip1.ImageScalingSize = new Size(20, 20);
+            menuStrip1.Items.AddRange(new ToolStripItem[] { fileToolStripMenuItem, editToolStripMenuItem, viewToolStripMenuItem, projectToolStripMenuItem });
+            menuStrip1.Location = new Point(0, 595);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(893, 28);
+            menuStrip1.TabIndex = 2;
+            menuStrip1.Text = "menuStrip1";
+            
+            // 
+            // fileToolStripMenuItem
+            // 
+            fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            fileToolStripMenuItem.Size = new Size(46, 24);
+            fileToolStripMenuItem.Text = "&File";
+            // 
+            // editToolStripMenuItem
+            // 
+            editToolStripMenuItem.Name = "editToolStripMenuItem";
+            editToolStripMenuItem.Size = new Size(49, 24);
+            editToolStripMenuItem.Text = "&Edit";
+            // 
+            // viewToolStripMenuItem
+            // 
+            viewToolStripMenuItem.Name = "viewToolStripMenuItem";
+            viewToolStripMenuItem.Size = new Size(55, 24);
+            viewToolStripMenuItem.Text = "&View";
+            // 
+            // projectToolStripMenuItem
+            // 
+            projectToolStripMenuItem.Name = "projectToolStripMenuItem";
+            projectToolStripMenuItem.Size = new Size(69, 24);
+            projectToolStripMenuItem.Text = "&Project";
+            // 
+            // toolStrip1
+            // 
+            toolStrip1.Dock = DockStyle.Bottom;
+            toolStrip1.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            toolStrip1.ImageScalingSize = new Size(20, 20);
+            toolStrip1.Location = new Point(0, 570);
+            toolStrip1.Name = "toolStrip1";
+            toolStrip1.Size = new Size(782, 25);
+            toolStrip1.TabIndex = 3;
+            toolStrip1.Text = "toolStrip1";
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.White;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(893, 623);
+            Controls.Add(toolStrip1);
+            Controls.Add(panel1);
+            Controls.Add(flowLayoutPanel1);
+            Controls.Add(menuStrip1);
             Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            MainMenuStrip = menuStrip1;
             Name = "Form1";
             Palette = kryptonPalette1;
             PaletteMode = ComponentFactory.Krypton.Toolkit.PaletteMode.Custom;
@@ -59,11 +206,29 @@
             StateCommon.Border.Rounding = 14;
             Text = "New Untitled Project";
             Load += Form1_Load;
+            flowLayoutPanel1.ResumeLayout(false);
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
         private ComponentFactory.Krypton.Toolkit.KryptonPalette kryptonPalette1;
+        private FlowLayoutPanel flowLayoutPanel1;
+        private ComponentFactory.Krypton.Toolkit.KryptonButton kryptonButton1;
+        private ComponentFactory.Krypton.Toolkit.KryptonButton kryptonButton2;
+        private ComponentFactory.Krypton.Toolkit.KryptonButton kryptonButton3;
+        private ComponentFactory.Krypton.Toolkit.KryptonButton kryptonButton4;
+        private ComponentFactory.Krypton.Toolkit.KryptonButton kryptonButton5;
+        private ComponentFactory.Krypton.Toolkit.KryptonButton kryptonButton6;
+        private Panel panel1;
+        private MenuStrip menuStrip1;
+        private ToolStrip toolStrip1;
+        private ToolStripMenuItem fileToolStripMenuItem;
+        private ToolStripMenuItem editToolStripMenuItem;
+        private ToolStripMenuItem viewToolStripMenuItem;
+        private ToolStripMenuItem projectToolStripMenuItem;
     }
 }
