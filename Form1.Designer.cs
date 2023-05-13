f43e4fabdc5dd678610bd574cb156454d8ecbc15
ajai <ajaikumarvs@gmail.com>
2023-05-13 09:27:27 +0530

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,69 @@
+﻿namespace Sirius
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            kryptonPalette1 = new ComponentFactory.Krypton.Toolkit.KryptonPalette(components);
+            SuspendLayout();
+            // 
+            // kryptonPalette1
+            // 
+            kryptonPalette1.FormStyles.FormMain.StateCommon.Back.Color1 = Color.White;
+            kryptonPalette1.FormStyles.FormMain.StateCommon.Back.Color2 = Color.White;
+            kryptonPalette1.FormStyles.FormMain.StateCommon.Border.DrawBorders = ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Top | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Bottom | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Left | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Right;
+            kryptonPalette1.FormStyles.FormMain.StateCommon.Border.GraphicsHint = ComponentFactory.Krypton.Toolkit.PaletteGraphicsHint.None;
+            kryptonPalette1.FormStyles.FormMain.StateCommon.Border.Rounding = 16;
+            kryptonPalette1.HeaderStyles.HeaderForm.StateCommon.Border.Color1 = Color.White;
+            kryptonPalette1.HeaderStyles.HeaderForm.StateCommon.Border.Color2 = Color.White;
+            kryptonPalette1.HeaderStyles.HeaderForm.StateCommon.Border.DrawBorders = ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Top | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Bottom | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Left | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Right;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.White;
+            ClientSize = new Size(800, 450);
+            Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            Name = "Form1";
+            Palette = kryptonPalette1;
+            PaletteMode = ComponentFactory.Krypton.Toolkit.PaletteMode.Custom;
+            ShowIcon = false;
+            StateCommon.Border.DrawBorders = ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Top | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Bottom | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Left | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Right;
+            StateCommon.Border.GraphicsHint = ComponentFactory.Krypton.Toolkit.PaletteGraphicsHint.None;
+            StateCommon.Border.Rounding = 14;
+            Text = "New Untitled Project";
+            Load += Form1_Load;
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private ComponentFactory.Krypton.Toolkit.KryptonPalette kryptonPalette1;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,18 @@
+using ComponentFactory.Krypton.Toolkit;
+namespace Sirius
+{
+    public partial class Form1 : KryptonForm
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+    }
+}
